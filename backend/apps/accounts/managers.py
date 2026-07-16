ade1773c081c484d273a5f94cb85340b8ed8b0df
Niteshpanday05 <eagleseye.np@gmail.com>
2026-07-16 16:13:06 +0545

    implementing authentication system

---
+++ backend/apps/accounts/managers.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email: str, password: str = None, **extra_fields):
+        if not email:
+            raise ValueError("Email address is required.")
+
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **extra_fields)
+
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email: str, password: str, **extra_fields):
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_verified", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError("Superuser must have is_staff=True.")
+
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
